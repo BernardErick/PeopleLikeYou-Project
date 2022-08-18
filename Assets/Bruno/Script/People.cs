a97d05c5cc9ee19f7f0fc16aa606104560c10c7e
Carlos Bruno <49172271+CBrunoSantos@users.noreply.github.com>
2022-08-17 22:26:34 -0300

    Dialogo com npc

--- Assets/Bruno/Script/People.cs
+++ Assets/Bruno/Script/People.cs
@@ -6,14 +6,40 @@ public class People : MonoBehaviour
 {
     public Rigidbody2D rig;
     public float speed;
-    void Start()
+    public Sprite profile;
+    public string speechTxt;
+    public string actorName;
+    public LayerMask playerLayer;
+    public float radious;
+
+    private DialogueControl dc;
+    bool onRadious;
+
+    private void Start()
     {
-        rig = GetComponent<Rigidbody2D>();
+        // rig = GetComponent<Rigidbody2D>();
+
+        dc = FindObjectOfType<DialogueControl>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        rig.velocity = new Vector2(speed, rig.velocity.y);
+        // rig.velocity = new Vector2(speed, rig.velocity.y);
+        if(Input.GetKeyDown(KeyCode.Space) && onRadious){
+            dc.Speech(profile,speechTxt, actorName);
+        }
+    }
+
+    private void FixedUpdate(){
+        Interact();
+    }
+
+    public void Interact(){
+        Collider2D hit = Physics2D.OverlapCircle(transform.position,radious,playerLayer);
+        if(hit != null){
+            onRadious = true;
+        } else{
+            onRadious = false;
+        }
     }
 }
